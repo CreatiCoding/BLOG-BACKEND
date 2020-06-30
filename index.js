ae46fa08b9cb949a687c15e3fcb753a1dca395ba
CreatiCoding <creaticoding@gmail.com>
2020-06-30 14:18:10 +0000

    feat: post read and create
    Signed-off-by: CreatiCoding <creaticoding@gmail.com>

---
+++ index.js
@@ -0,0 +1,64 @@
+const service = require("restana")();
+const bodyParser = require("body-parser");
+service.use(bodyParser.json());
+
+const mysql = require("mysql");
+
+const connectionLimit = process.env.DATABASE_CONNECTION_LIMIT;
+const host = process.env.DATABASE_HOST;
+const user = process.env.DATABASE_USER;
+const password = process.env.DATABASE_PASSWORD;
+const database = process.env.DATABASE_NAME;
+if (!connectionLimit || !host || !user || !password || !database) {
+  throw new Error("server parameter errors!");
+}
+
+const pool = mysql.createPool({
+  connectionLimit,
+  host,
+  user,
+  password,
+  database
+});
+const query = (sql, params = []) => {
+  try {
+    return new Promise((resolve, reject) => {
+      pool.query(sql, params, function(error, results, fields) {
+        if (error) reject(error);
+        resolve(results);
+      });
+    });
+  } catch (e) {
+    return e;
+  }
+};
+
+service.get("/vundle", (req, res) => {
+  res.send(
+    "wget -O - https://s3.ap-northeast-2.amazonaws.com/cdn.fedev.kr/script/vundle.sh | bash\n"
+  );
+});
+
+service.get("/version", (req, res) => {
+  res.send({ version: "1.0.0" });
+});
+
+service.get("/post/detail", async (req, res) => {
+  const { post_id } = req.query;
+  res.send({
+    result: await query(`SELECT * FROM POST WHERE post_id = ${post_id}`)
+  });
+});
+
+service.post("/post/create", async (req, res) => {
+  const body = req.body;
+  const { title, contents } = req.body;
+  res.send({
+    result: await query(
+      `INSERT INTO POST ( title, contents, created_at ) VALUES ( ?, ?, NOW() );`,
+      [title, contents]
+    )
+  });
+});
+
+service.start(4000).then(server => {});
